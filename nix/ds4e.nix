4d6bb3052d02e028870bc613c14095fccc42c46a
eikek <eike.kettner@posteo.de>
2024-01-25 21:33:04 +0100

    Adopt nix build to 23.11

--- nix/ds4e.nix
+++ nix/ds4e.nix
@@ -1,15 +1,15 @@
-{ lib, emacsPackagesNg }:
+{ lib, emacs }:
 
 let
   version =
     lib.fileContents ../version.txt;
 in
-emacsPackagesNg.trivialBuild rec {
+emacs.pkgs.trivialBuild rec {
   pname = "ds4e";
   inherit version;
   src = ../lisp;
   patchPhase = ''
     sed -i 's,ds4e-version "unknown",ds4e-version "${version}",g' ds4e-settings.el
   '';
-  packageRequires = with emacsPackagesNg; [ dashboard ];
+  packageRequires = with emacs.pkgs; [ dashboard ];
 }
