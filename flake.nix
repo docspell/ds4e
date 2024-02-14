034b1544857c33d65aeda5bd422aff802eb68e93
eikek <eike.kettner@posteo.de>
2024-02-14 22:01:15 +0100

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -3,19 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ flake-parts.flakeModules.easyOverlay ];
-
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
-
-      perSystem = { config, pkgs, ... }: {
-        packages = rec {
-          ds4e = pkgs.callPackage (import ./nix/ds4e.nix) {};
-          default = ds4e;
-        };
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
+    {
+      overlays.default = final: prev: {
+        ds4e = final.pkgs.callPackage (import ./nix/ds4e.nix) {};
       };
-    };
+     } // flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      packages = rec {
+        ds4e = pkgs.callPackage (import ./nix/ds4e.nix) {};
+        default = ds4e;
+      };
+    });
 }
